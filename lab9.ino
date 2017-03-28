6c70e92e4e5c739291e33b5a99bb18336f9441d4
sarahgill <sarahgill@gwu.edu>
2017-03-28 10:27:09 -0400

    Arduino file

---
+++ lab9.ino
@@ -0,0 +1,31 @@
+const int sensor = A0;
+int sensorRead;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensor, INPUT);
+}
+
+void loop() {
+  sensorRead = analogRead(sensor);
+  Serial.println(sensorRead);
+}
+
+void readInput(){
+  // String outgoingString;
+  // byte outgoingByte;
+  // byte outgoingByte2;
+
+  if(Serial.available() > 0){
+    Serial.println("WRITING");
+    // outgoingString = (String)sensor;
+    // outgoingByte = (byte)sensor;
+    // Send most significant byte first
+    /* outgoingByte2 = outgoingByte >> 8;
+    Serial.write(outgoingByte2);
+
+    // send least significant byte second
+    Serial.write(outgoingByte); */
+    Serial.print(sensorRead);
+  }
+}
